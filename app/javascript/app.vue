8bdcbaa5f1cd36979d5d399f37eee53be998fa23
masahito-nagayama <me18sf67m@gmail.com>
2021-02-10 15:35:35 +0900

    編集

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,32 +1,14 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
-    <ul>
-      <li v-for="memo in memos" :key="memo.id">
-        {{ memo.title }}: {{ memo.description }}
-      </li>
-    </ul>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-
 export default {
   data: function () {
     return {
-      memos: "memos"
-    }
-  },
-  mounted () {
-    this.setMemo();
-  },
-  methods: {
-    setMemo: function () {
-      axios.get('/api/memos')
-      .then(response=>(
-        this.memos = response.data
-      ))
+      message: "Hello Vue!"
     }
   }
 }
